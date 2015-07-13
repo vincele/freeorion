13cb7306474b1341f7b282d0d89425976a5356a5
Vincent Legoll <vincent.legoll@gmail.com>
2015-07-13 16:38:21 +0200

    Extract suitability computing related code
    from EncyclopediaDetailPanel into its own file and header
    in order to make it more suitable for reuse elsewhere
    
    MSVC project updates, grooming, passing by const reference
    by Geoff
    
    Signed-off-by: Vincent Legoll <vincent.legoll@gmail.com>

---
+++ universe/Suitability.h
@@ -0,0 +1,27 @@
+// -*- C++ -*-
+#ifndef _Suitability_h_
+#define _Suitability_h_
+
+#include "Enums.h"
+#include "TemporaryPtr.h"
+
+#include "../util/Export.h"
+
+#include <set>
+#include <map>
+#include <string>
+
+class Empire;
+class Planet;
+
+///< get a set of all the species from the given empire that can colonize this planet
+FO_COMMON_API std::set<std::string> GetColonizerSpecies(Empire* empire, TemporaryPtr<const Planet> planet, bool all_species = true);
+
+///< get the species, suitability pairs for all relevant species of the given empire and planet
+FO_COMMON_API std::map<std::string, std::pair<PlanetEnvironment, float> > GetSuitabilitiesForSpecies(int empire_id, int planet_id,
+                                                                                                     const std::set<std::string>& species_names);
+
+///< choose the best suitable species from the given set
+FO_COMMON_API std::string GetBestSuitable(const std::map<std::string, std::pair<PlanetEnvironment, float> >& suitabilities);
+
+#endif // _Suitability_h_
